d906e01106e21255f14417d425b1eb2369f26a86
David Bagaryan <bagaryan_ds@magnit.ru>
2026-03-10 16:30:33 +0300

    refactor

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"errors"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/bagardavidyanisntreal/tempobot/internal/service"
@@ -15,12 +19,34 @@ import (
 )
 
 func main() {
-	token := os.Getenv("BOT_TOKEN")
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+
+	token := os.Getenv("API_TOKEN")
+	if len(token) == 0 {
+		log.Printf("API_TOKEN env variable not set")
+
+		return
+	}
+
 	dbURL := os.Getenv("DB_URL")
+	if len(dbURL) == 0 {
+		log.Printf("DB_URL env variable not set")
+
+		return
+	}
 
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("failed get DB connect: %s", err)
+
+		return
+	}
+
+	if err = db.PingContext(ctx); err != nil {
+		log.Printf("failed ping DB: %s", err)
+
+		return
 	}
 
 	userRepo := storage.NewUserRepo(db)
@@ -32,26 +58,51 @@ func main() {
 
 	tg := telegram.NewClient(token)
 
-	handler := telegram.NewHandler(userService, eventService, tg)
+	tgHandler := telegram.NewHandler(userService, eventService, tg)
 
-	http.HandleFunc("/webhook", handler.HandleWebhook)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/live", func(w http.ResponseWriter, r *http.Request) {
+		if err = db.PingContext(r.Context()); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	})
+
+	mux.HandleFunc("/webhook", tgHandler.HandleWebhook)
 
 	log.Println("server started :8080")
 
 	//nolint:exhaustruct,mnd
 	server := &http.Server{
-		Addr:              ":8080",
-		ReadTimeout:       5 * time.Second,  // время на чтение заголовков и тела
-		WriteTimeout:      10 * time.Second, // время на запись ответа
-		IdleTimeout:       15 * time.Second, // время между запросами (keep-alive)
-		ReadHeaderTimeout: 2 * time.Second,  // ограничение времени на чтение заголовков
+		Addr:        ":8080",
+		Handler:     mux,
+		ReadTimeout: 5 * time.Second,
 	}
 
-	log.Println("server started :8080")
+	go func() {
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("server listen: %s", err)
+		}
+	}()
 
-	// Используем метод сервера вместо глобальной функции
-	err = server.ListenAndServe()
-	if err != nil {
-		log.Fatal(err)
+	<-ctx.Done()
+	log.Println("shutting down...")
+
+	//nolint: mnd
+	sdCtx, sdCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer sdCancel()
+
+	if err := server.Shutdown(sdCtx); err != nil {
+		log.Printf("server shutdown failed: %s", err)
+
+		return
+	}
+
+	if err := db.Close(); err != nil {
+		log.Printf("db close error: %s", err)
 	}
 }
